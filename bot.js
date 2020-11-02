ec99d65eeb65c232c9f1a6c83261ac1615891586
bastiengrignon <basgrignon@gmail.com>
2020-11-02 12:12:35 +0100

    Add tts to the messages

--- bot.js
+++ bot.js
@@ -2,17 +2,18 @@ const Discord = require('discord.js')
 const auth = require('./auth.json')
 
 const leavingMessage = (userId, channelName) => {
-    return `Dites au revoir à <@${userId}> il vient de quitter le channel ${channelName}`
+    return `<@${userId}> du channel ${channelName}`
 }
 
 const enteringMessage = (userId, channelName) => {
-    return `Dites bonjour à <@${userId}> il vient d'arriver dans le channel ${channelName}`
+    return `<@${userId}> arrive en petite tenue dans le channel ${channelName}`
 }
 
 const client = new Discord.Client()
-client.login(auth.token).then()
+client.login(process.env.BOT_TOKEN).then()
 
 client.on("ready", () => {
+    console.log("Online")
     client.user.setPresence({
         game: {
             name: "Coding",
@@ -26,10 +27,10 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     const channel = client.channels.cache.filter((channel) => channel.id === "772433272449007626").first()
 
     if (oldState.channel === null && newState.channel !== null) {
-        channel.send(enteringMessage(newState.member.user.id, newState.channel.name))
+        channel.send(enteringMessage(newState.member.user.id, newState.channel.name), {tts: true})
     }
     if (oldState.channel !== null && newState.channel === null) {
-        channel.send(leavingMessage(oldState.member.user.id, oldState.channel.name))
+        channel.send(leavingMessage(oldState.member.user.id, oldState.channel.name), {tts: true})
     }
 })
 
